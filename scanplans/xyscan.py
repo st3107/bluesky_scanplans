f999060a25662bf87dc163140ab45e38e4ccb3b5
Songsheng Tao <st3107@columbia.edu>
2020-02-11 18:22:30 -0500

    DEV: make the plan as an argument

--- scanplans/xyscan.py
+++ scanplans/xyscan.py
@@ -4,14 +4,17 @@ from xpdacq.beamtime import xpd_configuration, Beamtime, Sample
 from scanplans.mdgetters import *
 from typing import List
 
+__all__ = [
+    "xyscan"
+]
 
-def xyscan(bt, sample_index):
+
+def xyscan(bt, sample_index, plan_index):
     """
     Yield messages to count the predefined measurement plan on the a list of samples on a sample rack. It requires
     the following information to be added for each sample.
         position_x The x position of the sample in mm.
         position_y The y position of the sample in mm.
-        plan_ind The index of the plan in the Beamtime.
         wait_time The waiting time between the end of the former plan and the start of the latter plan in second.
 
     Parameters
@@ -19,7 +22,9 @@ def xyscan(bt, sample_index):
     bt: Beamtime
         The Beamtime instance that contains the sample information.
     sample_index : List[int]
-        A list of sample index in the BeamTime instance.
+        A list of the sample index in the BeamTime instance.
+    plan_index: List[int]
+        A list of the plan index in the BeamTime instance.
 
     Yields
     ------
@@ -33,7 +38,7 @@ def xyscan(bt, sample_index):
         >>> xpd_configuration["posy_controller"] = Grid_Y
     Register the scan plan to the beamtime.
         >>> ScanPlan(bt, ct, 30)
-    Add the information of 'position_x', 'position_y', 'plan_indice' and 'wait_time' to the excel and import.
+    Add the information of 'position_x', 'position_y' and 'wait_time' to the excel and import.
     Automatically conduct the scan plan for sample No.0 and No.1
         >>> plan = xyscan(bt, [0, 1])
         >>> xrun({}, plan)
@@ -41,11 +46,10 @@ def xyscan(bt, sample_index):
     posx_controller = xpd_configuration["posx_controller"]
     posy_controller = xpd_configuration["posy_controller"]
 
-    for sample_ind in sample_index:
+    for sample_ind, plan_ind in zip(sample_index, plan_index):
         sample = translate_to_sample(bt, int(sample_ind))
         posx = get_from_sample(sample, "position_x")
         posy = get_from_sample(sample, "position_y")
-        plan_ind = get_from_sample(sample, "plan_indice")
         wait_time = get_from_sample(sample, "wait_time")
         count_plan = translate_to_plan(bt, int(plan_ind), sample)
         if posx and posy and wait_time and count_plan:
