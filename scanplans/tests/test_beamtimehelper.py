14b28e92a5b31e14103f4660b87d34e91dc65de8
Songsheng Tao <st3107@columbia.edu>
2020-02-17 17:59:47 -0500

    TST: Add test for beamtimehelper.py

---
+++ scanplans/tests/test_beamtimehelper.py
@@ -0,0 +1,47 @@
+from ophyd.sim import SynAxis
+from bluesky import RunEngine
+from bluesky.plan_stubs import sleep
+from xpdacq.xpdacq_conf import xpd_configuration
+from xpdacq.beamtime import Beamtime, Sample, ScanPlan
+from scanplans.beamtimehelper import BeamtimeHelper
+from typing import Generator
+
+RE = RunEngine()
+motor0 = SynAxis(name="motor0")
+motor1 = SynAxis(name="moror1")
+xpd_configuration["posx_controller"] = motor0
+xpd_configuration["posy_controller"] = motor1
+xpd_configuration["area_det"] = None
+bt = Beamtime("Billinge", 300000, ["Billinge"])
+
+
+def test_beamtimehelper():
+    sample_md = {"sample_name": "Ni", "sample_composition": {"Ni": 1}}
+    Sample(bt, sample_md)
+
+    def plan(dets, sleep_time):
+        yield from sleep(sleep_time)
+
+    ScanPlan(bt, plan, 10)
+    bthelper = BeamtimeHelper(bt)
+    output_sample_md = bthelper.get_sample(0)
+
+    assert output_sample_md.get("sample_name") == "Ni"
+    assert output_sample_md.get("sample_composition") == {"Ni": 1}
+    assert isinstance(bthelper.get_plan(0), Generator)
+    assert bthelper.print_sample(0) is None
+    assert bthelper.print_plan(0) is None
+
+    x0, y0 = float(motor0.position), float(motor1.position)
+
+    RE(bthelper.aim_at_sample(0))
+    assert motor0.position == x0
+    assert motor1.position == y0
+    sample_md.update({
+        "position_x": x0 + 1,
+        "position_y": y0 + 1
+    })
+    Sample(bt, sample_md)
+    RE(bthelper.aim_at_sample(0))
+    assert motor0.position == x0 + 1
+    assert motor1.position == y0 + 1
