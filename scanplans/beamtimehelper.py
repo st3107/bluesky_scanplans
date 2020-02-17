f4b720c7848e9068805461639bc3f1a9e64df8a4
Songsheng Tao <st3107@columbia.edu>
2020-02-17 18:00:11 -0500

    BUG: debug the beamtimehelper.py

--- scanplans/beamtimehelper.py
+++ scanplans/beamtimehelper.py
@@ -1,9 +1,10 @@
 """A class to print sample information and generate bluesky plan to target samples."""
-from xpdacq.beamtime import Beamtime
+from xpdacq.beamtime import Beamtime, ScanPlan
 from xpdacq.xpdacq_conf import xpd_configuration
 from bluesky.plan_stubs import mv, null
+from bluesky.simulators import summarize_plan
 from pprint import pprint
-from typing import Union, Tuple
+from typing import Union, Tuple, Generator
 
 __all__ = [
     "BeamtimeHelper"
@@ -21,9 +22,9 @@ class BeamtimeHelper:
     Attributes
     ----------
     _bt
-        The instance storing meta data of the sample and plan.
+        The instance storing meta data of the sample and plan
     _pos_key
-        The key for the position of samples. Default is the global variable POS_KEYS.
+        The key for the position of samples. Default is the global variable POS_KEYS
     """
     def __init__(self, bt: Beamtime, pos_key: Tuple[str, str] = POS_KEYS):
         """
@@ -32,21 +33,21 @@ class BeamtimeHelper:
         Parameters
         ----------
         bt
-            The instance storing meta data of the sample and plan.
+            The instance storing meta data of the sample and plan
         pos_key
-
+            (Optional) The keys of the horizontal position and vertical position fields. Default POS_KEY
         """
         self._bt = bt
         self._pos_key = pos_key
 
-    def get_sample_meta(self, sample: Union[int, str]) -> dict:
+    def get_sample(self, sample: Union[int, str]) -> dict:
         """
         Get metadata of a sample.
 
         Parameters
         ----------
         sample
-            The sample index or sample name key.
+            The sample index or sample name key
 
         Returns
         -------
@@ -69,24 +70,72 @@ class BeamtimeHelper:
         Parameters
         ----------
         samples
-            The sample index or sample name key.
+            The sample index or sample name key
         """
         for sample in samples:
-            sample_meta = self.get_sample_meta(sample)
+            sample_meta = self.get_sample(sample)
             pprint(sample_meta)
+
+    def get_plan(self, plan: Union[int, str]) -> Generator:
+        """
+        Get the plan (message generator).
+
+        Parameters
+        ----------
+        plan
+            The plan index or plan name key
+
+        Returns
+        -------
+        plan_gen
+            The plan message generator.
+        """
+        if isinstance(plan, str):
+            plan_cls = self._bt.scanplans[plan]  # type: ScanPlan
+        elif isinstance(plan, int):
+            plan_cls = list(self._bt.scanplans.values())[plan]  # type: ScanPlan
+        else:
+            raise ValueError(f"{plan} is not int or str. It is {type(plan)}.")
+        plan_gen = plan_cls.factory()
+        return plan_gen
+
+    def print_plan(self, *plans: Union[int, str]):
+        """
+        Print the plan information.
+
+        Parameters
+        ----------
+        plans
+            The plan index or plan name key
+        """
+        for plan in plans:
+            plan_gen = self.get_plan(plan)
+            summarize_plan(plan_gen)
 
     def aim_at_sample(self, sample):
         """
-        Target the beam at the sample according to sample position metadata.
+        A generator of message: move the sample to the beam spot according to sample position metadata.
 
         Parameters
         ----------
         sample
-            The sample index or sample name key.
+            The sample index or sample name key
+
+        Examples
+        --------
+        Initiate a BeamtimeHelper.
+        >>> bthelper = BeamtimeHelper(bt)
+        Check the motors.
+        >>> xpd_configuration["posx_controller"]
+        >>> xpd_configuration["posy_controller"]
+        Aim at the sample of index 0 in bt.
+        >>> RE(bthelper.aim_at_sample(0))
+        Aim at the sample "Ni" in bt.
+        >>> RE(bthelper.aim_at_sample("Ni"))
         """
         posx_controller = xpd_configuration["posx_controller"]
         posy_controller = xpd_configuration["posy_controller"]
-        sample_meta = self.get_sample_meta(sample)
+        sample_meta = self.get_sample(sample)
         name = sample_meta.get("sample_name")
         print(f"INFO: Target sample {name}")
         pos_x_key, pos_y_key = self._pos_key
@@ -102,4 +151,4 @@ class BeamtimeHelper:
         else:
             print(f"INFO: Move to y = {pos_y}")
             yield from mv(posy_controller, float(pos_y))
-        yield null()
+        yield from null()
