506d05642109dfedc3e2c3407d45977c5316b4da
st3107 <st3107@columbia.edu>
2020-08-26 14:25:41 -0400

    MNT: refactor the package

---
+++ docs/source/usage.rst
@@ -0,0 +1,9 @@
+=====
+Usage
+=====
+
+Start by importing bluesky_scanplans.
+
+.. code-block:: python
+
+    import scanplans
--- setup.py
+++ setup.py
@@ -1,22 +1,61 @@
-import setuptools
+from os import path
+from setuptools import setup, find_packages
+import sys
+import versioneer
 
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# NOTE: This file must remain Python 2 compatible for the foreseeable future,
+# to ensure that we error out properly for people with outdated setuptools
+# and/or pip.
+min_version = (3, 7)
+if sys.version_info < min_version:
+    error = """
+scanplans does not support Python {0}.{1}.
+Python {2}.{3} and above is required. Check your Python version like so:
 
-setuptools.setup(
-    name="scanplans",
-    version="0.0.1",
-    author="Billinge group",
-    author_email="sb2896@columbia.edu",
-    description="A package of bluesky plans",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Billingegroup/bluesky_scanplans",
-    packages=setuptools.find_packages(),
+python3 --version
+
+This may be due to an out-of-date pip. Make sure you have pip >= 9.0.1.
+Upgrade pip like so:
+
+pip install --upgrade pip
+""".format(*(sys.version_info[:2] + min_version))
+    sys.exit(error)
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+
+setup(
+    name='scanplans',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Bluesky plans from Billinge group",
+    long_description=readme,
+    author="Songsheng Tao",
+    author_email='st3107@columbia.edu',
+    url='https://github.com/st3107/bluesky_scanplans',
+    python_requires='>={}'.format('.'.join(str(n) for n in min_version)),
+    packages=find_packages(exclude=['docs', 'tests']),
+    entry_points={
+        'console_scripts': [
+            # 'command = some.module:some_function',
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        'scanplans': [
+            # When adding files here, remember to update MANIFEST.in as well,
+            # or else they will not be included in the distribution on PyPI!
+            # 'path/to/data_file',
+        ]
+    },
+    license="BSD (3-clause)",
     classifiers=[
-        "Programming Language :: Python :: 3"
-        "License :: Other/Proprietary License"
+        'Development Status :: 2 - Pre-Alpha',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
     ],
-    python_requires='>=3.6',
 )
