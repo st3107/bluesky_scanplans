c815996601a9f2c40192f07832bd0c32a0994fd2
st3107 <st3107@columbia.edu>
2020-08-28 16:44:21 -0400

    DEV: add doc string

--- scanplans/move_and_do.py
+++ scanplans/move_and_do.py
@@ -16,6 +16,40 @@ def move_and_do_many(
         x_controller: str = "x_controller",
         y_controller: str = "y_controller",
 ) -> tp.List[tp.Generator]:
+    """Move to the sample and conduct the bluesky plan on the sample one by one.
+
+    Parameters
+    ----------
+    bt : Beamtime
+        The beamtime object.
+
+    sps : list
+        A list of (sample index, plan index). The index is shown in the 'bt.list()'.
+
+    wait_times : int or list
+        The wait time for all the samples.
+
+    wait_at_first : bool
+        Whether to wait before the plan is conducted for the first samples. If False, the plan will be conducted
+        immediately to the first sample no matter how wait_time is set.
+
+    sample_x : str
+        The key to the x position of the sample in the sample information. Default 'sample_y'.
+
+    sample_y : str
+        The key to the y position of the sample in the sample information. Default 'sample_x'.
+
+    x_controller : str
+        The key to the x position controller in `~xpdacq.beamtime.xqd_configuration`.
+
+    y_controller : str
+        The key to the x position controller in `~xpdacq.beamtime.xqd_configuration`.
+
+    Returns
+    -------
+    plans : list
+        A list of the bluesky plans. Each plan is a generator.
+    """
     if isinstance(wait_times, (int, float)):
         wait_times = [wait_times] * len(sps)
     else:
@@ -38,6 +72,7 @@ def move_and_do_one(
         sample_y: str = "sample_y", x_controller: str = "x_controller", y_controller: str =
         "y_controller"
 ) -> tp.Generator:
+    """Move to the sample and conduct the plan."""
     sample = translate_to_sample(bt, sample_ind)
     plan = translate_to_plan(bt, plan_ind, sample)
     xc = xpd_configuration[x_controller]
